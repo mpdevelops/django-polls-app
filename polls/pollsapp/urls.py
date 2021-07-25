0c49f8948235c866ffb627962e99ffd16827d052
Mike Paiva <mikepaiva@Mikes-MacBook-Air.local>
2021-07-25 11:27:37 -0400

    render questions and vote/results button

--- polls/pollsapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from . import views
-
-app_name = 'polls'
-urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('', views.index, name='index')
-]
--- polls/pollsapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from .models import Question, Choice
-
-# get questions and display them
-def index(request):
-    return render(request, 'polls/index.html')
